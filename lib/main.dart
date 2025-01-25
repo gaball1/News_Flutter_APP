61663c85564ae3211de9e043e0f2386ca95f9a27
AbdulrahmanGabal <abdelrahmangabal087@gmail.com>
2025-01-26 00:38:22 +0200

    final commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app_ui_setup/services/news_service.dart';
+import 'package:news_app_ui_setup/views/home_view.dart';
+import 'dart:io' show Platform;
+import 'dart:ui' show lerpDouble;
+void main() {
+  //NewsService(Dio()).getNews();
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
+  }
+}
